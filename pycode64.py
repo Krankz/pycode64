b74e0700430612f480ec9316fcaf08a19630225b
krankz <krankzdev@gmail.com>
2020-01-23 15:27:25 +1100

    added simple decoder argument

--- pycode64.py
+++ pycode64.py
@@ -5,6 +5,15 @@ def encoder(response):
     output = base64.b64encode(response.encode())
     output = output.decode() # removes b'...'
     print(output)
+
+def decoder(response):
+    output = base64.b64decode(response)
+    output = output.decode() #removes b'...'
+    print(output)
+
 if argv[1] == '-e':
     print('encoding...')
     encoder(argv[2])
+elif argv[1] == '-d':
+    print('decoding...')
+    decoder(argv[2])
