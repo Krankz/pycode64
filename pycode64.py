258612b372e1751a120d43f09d6315e6a11923b6
krankz <krankzdev@gmail.com>
2020-01-23 16:16:11 +1100

    added text file support

--- pycode64.py
+++ pycode64.py
@@ -2,13 +2,19 @@ import base64
 from sys import argv
 
 def encoder(response):
+    if response.endswith(".txt"):
+        f = open(response, 'r')
+        response = f.read()
     output = base64.b64encode(response.encode())
     output = output.decode() # removes b'...'
     print(output)
 
 def decoder(response):
+    if response.endswith(".txt"):
+        f = open(response, 'r')
+        response = f.read()
     output = base64.b64decode(response)
-    output = output.decode() #removes b'...'
+    output = output.decode() # removes b'...'
     print(output)
 
 if argv[1] == '-e':
